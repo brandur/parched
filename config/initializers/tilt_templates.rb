7c10127c18d82bed8f5cd7201ad5ba7c4905edf6
Brandur <brandur@mutelight.org>
2011-09-02 15:34:14 -0600

    * defining a new template is a little roundabout, just using Tilt::StringTemplate

--- config/initializers/tilt_templates.rb
+++ config/initializers/tilt_templates.rb
@@ -1,8 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Register custom Tilt templates here
-Tilt.register PassThroughTemplate, 'html'
-
-# Provides a template that simply passes content through to the other side.
-class PassThroughTemplate < Template
-end
+Tilt.register Tilt::StringTemplate, 'html'
