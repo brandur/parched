1a80ad0fb96161151920f033eb10bc00ea4b08de
Brandur <brandur@mutelight.org>
2011-09-04 22:31:42 -0600

    * helper for rendering partial pages (+ spec)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def partial_page(path)
+    repo = Skine::Repo.new(App.repo)
+    blob = repo.find(path)
+    blob = repo.find_fuzzy(path) unless blob
+
+    raise Error unless blob
+
+    # Tilt[] gets an appropriate template class given a file
+    klass = Tilt[blob.name]
+    Skine::Page.new(repo, klass, blob.data).render
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  describe 'partial_page' do
+
+    it 'should render partials' do
+      Skine::Repo.any_instance.should_receive(:find).and_return mockb('my/_partial blob') { |blob|
+        blob.should_receive(:name).and_return('my/_partial')
+        blob.should_receive(:data).and_return('Hello from a partial!')
+      }
+
+      partial_page('my/_partial').should == 'Hello from a partial!'
+    end
+
+    it 'should render partials that have a template' do
+      Skine::Repo.any_instance.should_receive(:find)
+      Skine::Repo.any_instance.should_receive(:find_fuzzy).and_return mockb('my/_partial blob') { |blob|
+        blob.should_receive(:name).and_return('my/_partial.md')
+        blob.should_receive(:data).and_return('Hello from a partial!')
+      }
+
+      partial_page('my/_partial').should == "<p>Hello from a partial!</p>\n"
+    end
+
+    it "should raise an error when given a partial that doesn't exist" do
+      Skine::Repo.any_instance.should_receive(:find)
+      Skine::Repo.any_instance.should_receive(:find_fuzzy)
+
+      expect{ partial_page('my/_partial') }.should raise_error
+    end
+
+  end
+
+end
